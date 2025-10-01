318b4d110b1451064a0313dd2a848524c081d188
alanchoi00 <alanchoi.uni@gmail.com>
2025-10-01 11:49:10 +1000

    refactor: clean up formatting and add semicolons in utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatUserName(firstName: string, lastName: string) {
+  return `${firstName} ${lastName}`;
+}
+
+export function formatInitials(firstName: string, lastName: string) {
+  return `${firstName.charAt(0)}${lastName.charAt(0)}`.toUpperCase();
 }
